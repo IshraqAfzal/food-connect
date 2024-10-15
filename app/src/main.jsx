6203ee4e1865165967be4493f4934ac3b19b7e64
Ishraq Afzal <ishraqafzal407@gmail.com>
2024-10-16 02:58:32 +0800

    major changes

--- app/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import HomeContainer from './components/home-container.jsx'
-import './input.css'
-import Login from './components/login.jsx'
-
-createRoot(document.getElementById('root')).render(
-    <Login/>
-)
