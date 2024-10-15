b2d8178775d4f0a034e153308a8c45544a062065
tamimsumaiya2002 <tamimsumaiya2002@gmail.com>
2024-10-16 05:15:53 +0800

    added routing functionalities

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import AdminNavbar from './components/admin-navbar'
 import AdminDashboard from './components/admin-dashboard.jsx'
 
+
 createRoot(document.getElementById('root')).render(
     <>
     <AdminNavbar></AdminNavbar>
