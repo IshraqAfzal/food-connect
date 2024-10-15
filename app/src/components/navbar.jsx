60a9967f559e07a459f2e3ffc2c1a8ff7ac2e083
tamimsumaiya2002 <tamimsumaiya2002@gmail.com>
2024-10-16 06:11:42 +0800

    minor changes

--- app/src/components/navbar.jsx
+++ app/src/components/navbar.jsx
@@ -1,6 +1,5 @@
 // import React from 'react'
 import './navbar.css'
-import { NavLink } from 'react-router-dom';
 
 function Navbar(){
     const handleHamburgerClick = () => {
@@ -19,17 +18,18 @@ function Navbar(){
             <div className="container">
             <div className="Header">
             <div className="Logo">FoodConnect</div>
-            <button className='Hamburger' onClick={handleHamburgerClick}> 
+            <button className='Hamburger' onClick={handleHamburgerClick}>&#9776;
             </button>
             </div>
 
             <nav>
                 <div className='Menu' style={{display:'none'}}>
                 <ul>
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/Contact">Contact</NavLink></li>
-                <li><NavLink to="/Register">Register</NavLink></li>
-                <li><NavLink to="/Login">Login</NavLink></li>
+                <li><a href="/">Home</a></li>
+                <li><a href="/contact">Contact</a></li>
+                <li><a href="/register">Register</a></li>
+                <li><a href="/login">Login</a></li>
+
             </ul>
                 </div>
                 
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,12 +2,10 @@ import { createRoot } from 'react-dom/client'
 import AdminNavbar from './components/admin-navbar'
 import Chatbot from './components/admin-chatbot.jsx'
 import CreateEvent from './components/admin-create-event'
-
-
+import Navbar from './components/navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
     <>
-    <AdminNavbar/>
-    <CreateEvent/>
+    <Navbar/>
     </>
 )
